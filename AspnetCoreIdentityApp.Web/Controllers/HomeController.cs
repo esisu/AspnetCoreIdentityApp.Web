dc85faca12995080d652ea568da0e5465113634b
ISTINYE\erkan.salihoglu <erkan.salihoglu@istinye.edu.tr>
2023-07-12 22:17:11 +0300

    Add project files.

---
+++ AspnetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using AspnetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using AspnetCoreIdentityApp.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace AspnetCoreIdentityApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel request)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var identityResult = await _userManager.CreateAsync(new()
+            {
+                UserName = request.Username,
+                PhoneNumber = request.Phone,
+                Email = request.Email,
+            }, request.ConfirmPassword);
+            
+            if (identityResult.Succeeded)
+            {
+                TempData["SuccessMessage"] = "Üyelik Kayıt işlemi başarı ile gerçekleşmiştir.";
+
+                return RedirectToAction(nameof(HomeController.SignUp));
+            }
+
+            foreach (var result in identityResult.Errors)
+            {
+                ModelState.AddModelError(String.Empty, result.Description);
+            }
+            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ AspnetCoreIdentityApp.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspnetCoreIdentityApp.Web.ViewModels
+{
+    public class SignUpViewModel
+    {
+
+        public SignUpViewModel()
+        {
+
+        }
+
+        public SignUpViewModel(string username, string email, string phone, string password, string confirmPassword)
+        {
+            Username = username;
+            Email = email;
+            Phone = phone;
+            Password = password;
+            ConfirmPassword = confirmPassword;
+        }
+
+        [Required(ErrorMessage = "Ad alanını Lütfen Boş Bırakmayınız")]
+        [Display(Name = "Kullanıcı Adı : ")]
+        public string Username { get; set; }
+
+        [EmailAddress(ErrorMessage = "Email Formatı yanlış kontrol ediniz")]
+        [Required(ErrorMessage = "Email alanını Lütfen Boş Bırakmayınız")]
+        [Display(Name = "E-Posta : ")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Telefon alanını Lütfen Boş Bırakmayınız")]
+        [Display(Name = "Telefon : ")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Şifre alanını Lütfen Boş Bırakmayınız")]
+        [Display(Name = "Şifre : ")]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password),ErrorMessage = "Şifreler aynı değil kontrol ediniz")]
+        [Required(ErrorMessage = "Şifre Tekrar alanını Lütfen Boş Bırakmayınız")]
+        [Display(Name = "Şifre Tekrar : ")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
